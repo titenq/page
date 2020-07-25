11063d7ed5cb24e41122668c81b7ef774cc07c81
titenq <titenq@gmail.com>
2020-07-24 22:21:06 -0300

    Updates

---
+++ static/js/7.d4154cf6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppage=this.webpackJsonppage||[]).push([[7],{39:function(e,n,t){"use strict";t.r(n);var a=t(0),u=t.n(a);n.default=function(){return u.a.createElement(u.a.Fragment,null,u.a.createElement("h1",null,"PageNotFound"))}}}]);
+//# sourceMappingURL=7.d4154cf6.chunk.js.map
